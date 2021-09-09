def71fa14a7ef1c4665b6dc6c6905c3d5b1201b1
Math2121 <matheusdepaula527@gmail.com>
2021-09-09 11:12:02 -0300

    criando documentação

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -10,31 +10,40 @@ class ImportCategoryUseCase {
   constructor(private catgeoriesRepository: ICategoriesRepository) {}
   loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
     return new Promise((resolve, reject) => {
-      const stream = fs.createReadStream(file.path)
-      const parseFile = csvParse({delimiter:";"});
+      const stream = fs.createReadStream(file.path);
+      const parseFile = csvParse({ delimiter: ";" });
       const categories: IImportCategory[] = [];
       stream.pipe(parseFile);
-      parseFile.on("data", async (line) => {
-        console.log(line);
-        const [name, description] = line;
-        categories
-          .push({
+      parseFile
+        .on("data", async (line) => {
+          console.log(line);
+          const [name, description] = line;
+          categories.push({
             name,
             description,
-          })
-        
-      })
-      .on("end", () => {
-        resolve(categories);
-      })
-      .on("error", (err) => {
-        reject(err);
-      });
+          });
+        })
+        .on("end", () => {
+          fs.promises.unlink(file.path)
+          resolve(categories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
     });
   }
   async execute(file: Express.Multer.File): Promise<void> {
     const categories = await this.loadCategories(file);
-    console.log(categories);
+
+    categories.map(async (category) => {
+      const { name, description } = category;
+
+      const existCategory = this.catgeoriesRepository.findByName(name);
+
+      if (!existCategory) {
+        this.catgeoriesRepository.create({ name, description });
+      }
+    });
   }
 }
 export { ImportCategoryUseCase };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import express from "express";
-
+import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
-
+import SwaggerFile from './swagger.json'
 const app = express();
 app.use(express.json());
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(SwaggerFile));
 app.use(router)
 app.listen(3333, () => {
   console.log("Server running");
