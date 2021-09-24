7be98b1e9887e5ce2f495aca29555444d9ee5b5b
Math2121 <matheusdepaula527@gmail.com>
2021-09-24 17:35:26 -0300

    add typorm

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
-import SwaggerFile from './swagger.json'
+import SwaggerFile from "./swagger.json";
+
+import "./database";
 const app = express();
 app.use(express.json());
-app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(SwaggerFile));
-app.use(router)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(SwaggerFile));
+app.use(router);
 app.listen(3333, () => {
   console.log("Server running");
 });
