3cf364ac02e9aa2ed6960f4837e60bac722f0cec
Math2121 <matheusdepaula527@gmail.com>
2021-09-27 08:02:33 -0300

    refactory repositories

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,8 +1,16 @@
 import {v4 as uuidv4} from 'uuid'
+import {Column, CreateDateColumn, Entity, PrimaryColumn} from "typeorm"
+
+@Entity("Categories")
 class Category {
+  @Column()
   name: string;
+  @Column()
   description: string;
+  @CreateDateColumn()
   created_at: Date;
+
+  @PrimaryColumn()
   id?: string;
 
   constructor() {
