ff4cf4a18266aac673c9af14efa2331f5aeafee9
Math2121 <matheusdepaula527@gmail.com>
2021-11-16 15:41:01 -0300

    start with tests

---
+++ src/modules/cars/useCases/createCategory/CreateCategory.spec.ts
@@ -0,0 +1,26 @@
+import { CategoriesRepositoryInMemory } from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+let createCategoryUseCase: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+describe("Create Category", () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+  });
+  it("Should be able to create a new category", async () => {
+    const category = {
+      name: "Category Test",
+      description: "description test",
+    };
+
+    await createCategoryUseCase.execute({
+      name: category.name,
+      description: category.description,
+    });
+
+    const categoryExists = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+    expect(categoryExists).toHaveProperty("id");
+  });
+});
