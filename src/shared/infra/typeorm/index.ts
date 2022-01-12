46557f4847ec24d93ed5cb0676aef16ad69a3c60
matheusdepaula <matheus@nvgo.com.br>
2022-01-12 16:05:41 -0300

    fazendo teste de integração em categorias

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,7 +1,15 @@
-import { createConnection, getConnectionOptions,Connection } from "typeorm";
+import { createConnection, getConnectionOptions, Connection } from "typeorm";
 
 export default async (host = "database"): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
-  return createConnection(Object.assign(defaultOptions, { host }));
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host: process.env.NODE_ENV === "test" ? "localhost" : host,
+      database:
+        process.env.NODE_ENV === "test"
+          ? "rentx_test"
+          : defaultOptions.database,
+    })
+  );
 };
